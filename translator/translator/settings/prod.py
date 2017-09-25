10df63c0ec5f9494fefe571fce540a41f0e74a02
ylu021 <yichen_0@hotmail.com>
2017-09-25 10:37:04 -0400

    deploy settings

--- translator/translator/settings/prod.py
+++ translator/translator/settings/prod.py
@@ -1,1 +1,20 @@
 from translator.settings.common import *
+
+import environ
+
+environ.Env.read_env('.env')
+#
+# CORS_ORIGIN_WHITELIST = (
+#     'localhost:3000',
+# )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['PROD_DB_NAME'],
+        'USER' : os.environ['PROD_DB_USER']    ,
+        'PASSWORD' : '12345678',
+        'HOST' : 'os.environ['PROD_DB_HOST']',
+        'PORT' : '5432',
+    }
+}
--- translator/translator/wsgi.py
+++ translator/translator/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translator.settings.prod")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translator.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translator.settings.prod")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translator.settings.local")
 
 application = get_wsgi_application()
